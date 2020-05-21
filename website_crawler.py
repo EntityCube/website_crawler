8409edf9cddcc178bc1dc5d0beb111d3dcb2d405
EntityCuber <entitycuber@gmail.com>
2020-05-21 07:50:56 +0530

    fixed relative branches

--- website_crawler.py
+++ website_crawler.py
@@ -7,6 +7,7 @@ import requests
 import time
 
 url = input("url: ")
+deep = 1
 
 # Add https if its not in url input
 if not url[0:8].lower() == "https://" or url[0:7].lower() == "http://":
@@ -21,15 +22,19 @@ links = []
 for link in soup.find_all('a'):
     links.append(link.get('href'))
 
-print(f'Found {len(links)} links on {url}')
-time.sleep(1)
 
+print(f'Found {len(links)} links on {url}')
+
+time.sleep(1)
 
 def clean_data(data):
     data = list(filter(None, data))
     for i, link in enumerate(data):
         data[i] = list(filter(None, link.split('/')))
         data[i].insert(0, '^')
+
+        if not (data[i][1] == 'https:' or data[i][1] == 'http:'):
+            data[i].insert(1, url)
     data = list(filter(None, data))
     data.sort(key=len, reverse=True)
     return data
@@ -58,3 +63,4 @@ if len(data):
 
     for pre, fill, node in RenderTree(sitetree):
         print(f"{pre}{node.name}")
+    print(data)
